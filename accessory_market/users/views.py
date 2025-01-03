abc03fd3c5a1c80c23cf964ebd68e5a0f7a3e8f5
MindlessMuse666 <mindlessmuse.666@gmail.com>
2025-01-03 19:52:37 +0300

    feat, fix, ux- ui-design*
    feat:
    1. Создана кастомная модель `User` с аватаркой и подвязана к БД.
    2. Создано view для User`а (`users\views.py`).
    3. Импортирован и интегрирован новый шрифт `animeace2_0bbcyr.otf`.
    
    fix:
    Исправлена ошибка, из-за которой у цены в `base.html` слетели значки доллара.
    
    ux- ui-design* (необходимо сделать):
    1. Иконку корзины.
    2. Иконки заднего фона для карточек товаров в корзине.
    3. Новые иконки кнопок в корзине (`Continue Shopping` и `Checkout`).
    4. Изображение продуктов. Описание этих продуктов.
    5. Аватарки тестовых пользователей.
    6. Подходящие аниме-шрифты.
    ---
    feat:
    1. A custom `User` model with an avatar has been created and linked to the database.
    2. A view has been created for the User (`users\views.py `).
    3. The new font `animeace2_0bbcyr.otf` has been imported and integrated.
    
    fix:
    Fixed a bug where the price in `base.html "the dollar badges flew off.
    
    ux- ui-design* (must be done):
    1. The basket icon.
    2. Background icons for product cards in the shopping cart.
    3. New icons for buttons in the shopping cart ('Continue Shopping` and `Checkout').
    4. Product images. Description of these products.
    5. Avatars of test users.
    6. Suitable anime-fonts.

--- accessory_market/users/views.py
+++ accessory_market/users/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def login(request: HttpRequest):
+    render_view: HttpResponse = render(
+        request,
+        'users/login.html'
+    )
+    
+    return render_view
+
+
+def registration(request: HttpRequest):
+    render_view: HttpResponse = render(
+        request,
+        'users/registration.html'
+    )
+    
+    return render_view
+
+
+def profile(request: HttpRequest):
+    render_view: HttpResponse = render(
+        request,
+        'users/profile.html'
+    )
+    
+    return render_view
+
+
+def logout(request: HttpRequest):
+    pass
+    # render_view: HttpResponse = render(
+    #     request,
+    #     'users/login.html'
+    # )
+    
+    # return render_view
